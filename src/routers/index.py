10d9a2907d59b0d2f0bceab09296048ae8341f5c
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2024-04-05 21:31:11 +0800

    feat: Added working options

--- src/routers/index.py
+++ src/routers/index.py
@@ -1,5 +1,7 @@
+import json
+
 from flask import Blueprint, request, render_template
-from folium import folium, Marker
+from folium import Map, Marker, GeoJson, folium
 
 from src.data.storages.WeatherDataInMemoryStorage import WeatherDataInMemoryStorage
 
@@ -11,27 +13,46 @@ def index():
     return render_template('index.html')
 
 
-@index_router.route('/generate_map', methods=['GET', 'POST'])
+@index_router.route('/generate_map', methods=['POST'])
 def generate_map():
-    if request.method == 'POST':
-        target_date = request.form['date']
+    target_date = request.form['date']
+    selected_options = request.form.getlist('options[]')
 
-        storage = WeatherDataInMemoryStorage()
-        parsed_data = storage.get_weather_data_by_date(target_date)
+    storage = WeatherDataInMemoryStorage()
+    parsed_data = storage.get_weather_data_by_date(target_date)
 
-        m = folium.Map(location=[52.17, 104.18], zoom_start=5)
-        for weather_data in parsed_data:
-            if weather_data.WW == " " or weather_data.WW_type == "":
-                continue
+    m = Map(location=[52.17, 104.18], zoom_start=5)
+
+    # Add rivers from rivers.geojson if "rivers" option is selected
+    if 'rivers' in selected_options:
+        rivers_data = 'data/data/rivers.geojson'
+        with open(rivers_data, encoding='utf-8') as f:
+            rivers_geojson = json.load(f)
+
+        GeoJson(
+            rivers_geojson,
+            name='geojson'
+        ).add_to(m)
+
+    if 'regions' in selected_options:
+        regions_data = 'data/data/MO_Irk_region_32648.geojson'
+        with open(regions_data, encoding='utf-8') as f:
+            regions_geojson = json.load(f)
+
+        GeoJson(
+            regions_geojson,
+            name='geojson'
+        ).add_to(m)
+
+    for weather_data in parsed_data:
+        if weather_data.WW == " " or weather_data.WW_type == "":
+            continue
+        if 'rivers' in selected_options and weather_data.type == 'river':
             Marker(
                 location=[weather_data.lat, weather_data.lon],
                 popup=f'Date: {target_date}\nInfo: {weather_data.WW}\nWeather: {weather_data.WW_type}'
             ).add_to(m)
 
-        map_html = m.get_root().render()
-        print("map готова")
-        return map_html
-    else:
-        m = folium.Map(location=[67.214, 153.327], zoom_start=5)
-        map_html = m.get_root().render()
-        return map_html
+    map_html = m.get_root().render()
+    print("map готова")
+    return map_html
