4ca94bfd39dbb79c72e8c4e2a4ac842a94f5c5cf
Fedor Kuznetsov <kuzneczov-f@susanoo.ru>
2024-04-05 20:15:07 +0800

    feat: Added temp index.html

--- src/data/storages/WeatherDataInMemoryStorage.py
+++ src/data/storages/WeatherDataInMemoryStorage.py
@@ -10,6 +10,9 @@ class WeatherDataInMemoryStorage:
     def add_weather_data(self, weather_data):
         self._data.append(weather_data)
 
+    def get_weather_data_by_date(self, date):
+        return [weather_data for weather_data in self._data if weather_data.date == date]
+
     def get_all_weather_data(self):
         return self._data
 
--- src/routers/index.py
+++ src/routers/index.py
@@ -1,8 +1,37 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
+from folium import folium, Marker
+
+from src.data.storages.WeatherDataInMemoryStorage import WeatherDataInMemoryStorage
 
 index_router = Blueprint('index', __name__)
 
 
 @index_router.route('/')
 def index():
-    return "Hello world!"
+    return render_template('index.html')
+
+
+@index_router.route('/generate_map', methods=['GET', 'POST'])
+def generate_map():
+    if request.method == 'POST':
+        target_date = request.form['date']
+
+        storage = WeatherDataInMemoryStorage()
+        parsed_data = storage.get_weather_data_by_date(target_date)
+
+        m = folium.Map(location=[52.17, 104.18], zoom_start=5)
+        for weather_data in parsed_data:
+            if weather_data.WW == " " or weather_data.WW_type == "":
+                continue
+            Marker(
+                location=[weather_data.lat, weather_data.lon],
+                popup=f'Date: {target_date}\nInfo: {weather_data.WW}\nWeather: {weather_data.WW_type}'
+            ).add_to(m)
+
+        map_html = m.get_root().render()
+        print("map готова")
+        return map_html
+    else:
+        m = folium.Map(location=[67.214, 153.327], zoom_start=5)
+        map_html = m.get_root().render()
+        return map_html
